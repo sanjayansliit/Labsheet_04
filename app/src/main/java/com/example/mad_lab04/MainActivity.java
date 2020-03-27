797ca8b56b144a9db84840bcca30782da676f2ba
Sanjayansliit <ravisanjayan97@gmail.com>
2020-03-27 21:14:34 +0530

    Labsheet 04 answer

---
+++ app/src/main/java/com/example/mad_lab04/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mad_lab04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = getString(R.string.app_name);
+
+            String description = getString(R.string.app_name);
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+}
